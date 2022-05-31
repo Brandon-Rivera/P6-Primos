1c73a4dc708fce6e68e70851c82017375da5b232
Brandon-Rivera <A01424338@itesm.mx>
2022-05-31 17:04:00 -0500

    Agregar secuencial con tiempo

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <chrono>
+#include <cmath>
 
 using namespace std;
 
+bool isPrime(int n) {
+  if (n == 0 || n == 1 || n == 4) return false;
+  for (int x = 2; x < n / 2; x++) {
+    if (n % x == 0) return false;
+  }
+  return true;
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int n = 50000, sum = 0;
+    bool IsPrime = false;
+
+    //Secuencial
+    auto start = chrono::high_resolution_clock::now();
+    for (int i = n; i > 0; i--)
+    {
+        IsPrime = isPrime(i);
+        if(IsPrime) sum += i; 
+    }
+
+    auto end = chrono::high_resolution_clock::now();
+    cout << "Suma = " << sum << endl;
+    cout << "Duración: " << chrono::duration_cast<chrono::microseconds>(end - start).count() << " microseconds" << endl;
+
+    //Paralelo
+    
+
     return 0;
 }
